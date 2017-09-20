1b44d28effe25ada27d48065e1d3b12e92b19875
Ashton Six <me@ashtonsix.com>
2017-09-20 05:16:06 +0100

    Fix bin for global install

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const getDetails = require('./src/utils/getDetails')
-const command = process.argv[process.argv.indexOf('fa') + 1]
+const command =
+  process.argv[process.argv.findIndex(arg => arg.endsWith('fa')) + 1]
 
 require(`./src/${command}`)(getDetails(command === 'setup'))
